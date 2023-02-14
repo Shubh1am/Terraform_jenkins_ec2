950dedd97ec9bcc22037acdc861b0c5a2cf6e2b5
Shubh1am <88609379+Shubh1am@users.noreply.github.com>
2023-02-14 16:27:00 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-        access_key=     "AKIASRU6EIGUZJWUJZTY"
-        secret_key=    "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"
+        access_key=     "AKIA22J5237JM3GHJFH3"
+        secret_key=    "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"
         region= var.region
  }
 
